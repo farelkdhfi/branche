f2e207bdf5b10a1b3319f518d31a82f573f9e20a
farelkdhfi <farelkadhafi34@gmail.com>
2025-02-21 13:27:39 +0700

    asas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className='bg-black flex flex-col justify-center items-center min-h-screen relative'>
             <h1 className='lg:text-[22vw] z-10 lg:z-0 text-white text-5xl font-thin font-script'>CATHARSIS</h1>
-            <img src={Img} alt="" className='absolute bottom-0 h-[85vh] w-full lg:h-[95vh]' />
+            <img src={Img} alt="" className='absolute bottom-0 h-[85vh] lg:h-[85vh]' />
             <p className='text-2xl text-white mt-5 z-10'>EMPIRE</p>
     </section>
   )
